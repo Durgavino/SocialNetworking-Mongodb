0ed6a04f26bd6529626b55b45efa5dcc30f294c8
Durgavino <mailtodurga93@gmail.com>
2023-03-16 15:20:10 -0300

    updated the userby id

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -13,5 +13,18 @@ createaNewUser(req,res){
     User.create(req.body)
     .then((user)=>res.json(user))
     .catch((err)=>res.status(500).json(err));
+},
+getUserbyId(req,res)
+{
+    User.findOne({_id:req.params.userId})
+        .then((user)=>
+    !user? res.status(404).json({message:'No user with that ID'})
+    :res.json(user)
+    )
+    .catch((err)=>res.status(500).json(err));
 }
+
+
+
+
 }
--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 
 const {
     getuser,
-    // getUserbyId,
+     getUserbyId,
      createaNewUser,
-    // updateaUserbyId,
+     updateaUserbyId,
     // deleteTheUserById,
 } = require('../../controllers/usercontroller.js');
 
 router.route('/').get(getuser).post(createaNewUser);
+
+router.route('/:userId').get(getUserbyId).put(updateaUserbyId);
 
 // router.route('/:userId').get(getUserbyId).put(updateaUserbyId).delete(deleteTheUserById);
 
